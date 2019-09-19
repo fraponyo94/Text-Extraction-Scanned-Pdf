5516019aea0d493b32e2c99c02ce6f04c7571799
Fredrick Aponyo <fraponyo94@gmail.com>
2019-09-19 14:49:09 +0300

    tesseract Ocr on pdf java

---
+++ src/main/java/tesseract/ocr/TesseractScannedPdfTextExtraction.java
@@ -0,0 +1,166 @@
+package tesseract.ocr;
+
+import com.recognition.software.jdeskew.ImageDeskew;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.ImageHelper;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.LinkedList;
+
+public class TesseractScannedPdfTextExtraction {
+    private Tesseract tesseracta = new Tesseract();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void extractTextFromScannedPdfWithTikaOCR(String fileName) throws IOException {
+        InputStream stream = null;
+        stream = new FileInputStream(fileName);
+
+
+    }
+
+
+    // Preprocess the bufferedImage
+    private BufferedImage correctSkewness(BufferedImage image) {
+
+        /*
+         * This method corrects skewness of the image, if necessary
+         */
+
+        final double MINIMUM_DESKEW_THRESHOLD = 0.05d;
+
+        ImageDeskew mImage = new ImageDeskew(image);
+        double imageSkewAngle = mImage.getSkewAngle(); // determine skew angle
+        if ((imageSkewAngle > MINIMUM_DESKEW_THRESHOLD || imageSkewAngle < -(MINIMUM_DESKEW_THRESHOLD))) {
+            image = ImageHelper.rotateImage(image, -imageSkewAngle); // deskew image
+        }
+
+        return image;
+    }
+
+
+
+    // Extract text from pdf images
+    private String extractTextFromImage(BufferedImage image,Tesseract tesseract) {
+
+        BufferedImage grayImage = ImageHelper.convertImageToGrayscale(image);
+        String ocrResults = null;
+        try {
+            ocrResults = tesseract.doOCR(grayImage).replaceAll("\\n{2,}", "\n");
+
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+
+        if (ocrResults == null || ocrResults.trim().length() == 0) {
+
+            return null;
+        }
+
+        ocrResults = ocrResults.trim();
+        // TODO remove the trash that doesn't seem to be words
+        return ocrResults;
+    }
+
+
+    /* Get byte arrays from inputstream
+     *  @ return byte[]
+     *
+     * */
+    private byte[] getArrayFromInputStream(InputStream inputStream) throws IOException {
+        byte[] bytes;
+        byte[] buffer = new byte[1024];
+        try(BufferedInputStream is = new BufferedInputStream(inputStream)){
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            int length;
+            while ((length = is.read(buffer)) > -1 ) {
+                bos.write(buffer, 0, length);
+            }
+            bos.flush();
+            bytes = bos.toByteArray();
+        }
+        return bytes;
+    }
+
+
+    /* Check for scanned pdf and return contained images
+     * @return LinkedList<BufferedImage>
+     * */
+    private LinkedList<BufferedImage> checkScannedPdf(File pdfFile ) throws IOException {
+        int images = 0;
+        int numberOfPages = 0;
+
+        LinkedList<BufferedImage> bufferedImages = new LinkedList<>();
+
+
+        PDDocument doc = PDDocument.load(pdfFile);
+
+        PDPageTree list = doc.getPages();
+
+        numberOfPages = doc.getNumberOfPages();
+
+        for (PDPage page : list) {
+
+            PDResources resource = page.getResources();
+
+            for (COSName xObjectName : resource.getXObjectNames()) {
+
+                PDXObject xObject = resource.getXObject(xObjectName);
+
+                if (xObject instanceof PDImageXObject) {
+                    PDImageXObject image = (PDImageXObject) xObject;
+
+                    BufferedImage bufferedImage = image.getImage();
+                    // Add bufferedImages to list
+                    bufferedImages.add(bufferedImage);
+                    images++;
+                }
+
+            }
+
+        }
+
+        doc.close();
+
+        //  pdf pages if equal to the images === scanned pdf ===
+        if (numberOfPages == images) {
+            return bufferedImages;
+        } else {
+
+            return new LinkedList<>();
+        }
+
+    }
+
+
+    // Extract text
+    private String extractText(byte[] bytes) {
+        StringBuilder extractedText = new StringBuilder("");
+        LinkedList<BufferedImage> bufferedImageList = new LinkedList<BufferedImage>();
+
+
+
+        return extractedText.toString();
+    }
+
+}
